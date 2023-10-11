3184b367bb464c3e41aca81433087b34780d4abe
Emalee Poellot <emalee.poellot@gmail.com>
2023-10-10 20:18:39 -0600

    Update createDeck function. Create takeTurn function. Create calculateCorrect function. create endRound function

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
 const { createCard } = require('../src/card');
+var percentageCorrect; 
+var currentIndex = 0;
 
 const evaluateGuess = (guess, cardObject) => {
-    //correctAnswer will be the createCard object 
     if (guess === cardObject.cardAnswer) {
         return 'Correct!';
     } else {
         return 'Incorrect!';
     }
-}
+};
 
-//iterate through the protype data invoking the createCard function on each index. create a deck
-const createDeck = cardsArray => {
-     const deck = [];
-     deck.join(cardsArray);
-     return deck;
-     //so that it can work more than once in the same dataset
-}
+const createRound = currentDeck => {
+    return {
+        deck: currentDeck, 
+        currentCard: currentDeck[currentIndex], 
+        incorrectGuesses: 0, //increment
+        incorrectGuessId: [],
+        turns: 0 //increment
+    }
+};
 
-const countCards = currentDeck => currentDeck.length;
+const takeTurn = (guess, roundObj) => {
+    roundObj.turns++;
+    if (guess === roundObj.currentCard.correctAnswer) {
+        console.log('Correct!');
+    } else {
+        roundObj.incorrectGuesses++;
+        roundObj.incorrectGuessId.push(currentCard.id);
+        console.log('Incorrect!');
+    }
+    currentIndex++;
+    return roundObj;
+};
 
+const calculatePercentCorrect = roundObj => {
+    percentageCorrect =  1 - (roundObj.incorrectGuesses/roundObj.turns);
+    return percentageCorrect; 
+}; //need to update this so it's a percentage not a decimal.
 
-console.log('Your project is running...'); 
+const endRound = roundObj => {
+    if (roundObj.turns === currentDeck.length) {
+        console.log(`Round over! You answered ${percentageCorrect} of the questions correctly!`);
+    }
+};
 
 module.exports = { 
-    evaluateGuess, 
-    createDeck,
-    countCards
+    evaluateGuess,
+    createRound,
+    takeTurn,
+    calculatePercentCorrect, 
+    endRound
  }
