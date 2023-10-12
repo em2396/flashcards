20106408ea90942c9b7da1a710ead20b495effd1
Emalee Poellot <emalee.poellot@gmail.com>
2023-10-12 14:56:53 -0600

    Complete testing for index files

--- test/index-test.js
+++ test/index-test.js
@@ -1,45 +1,58 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const  { evaluateGuess, calculatePercentCorrect } = require('../src/index');
-const  { createRound } = require('../src/index');
+const  {takeTurn , calculatePercentCorrect, createRound, endRound } = require('../src/index');
 const { createCard } = require('../src/card');
 const { start } = require('../src/game');
-
-describe('User guesses answer', function(){
-    it('should be a function', function(){
-        expect(evaluateGuess).to.be.a('function')
-    });
-    it('should return correct if answer matches answer', function(){
-        const userGuess = 'array';
-        const card2 = createCard(1);
-
-        expect(userGuess).to.equal(card2.correctAnswer)
-    });
-    it('should return incorrect if answer doesn\'t match answer', function(){
-        const userGuess = 'object';
-        const card2 = createCard(1);
-
-        expect(userGuess).to.not.equal(card2.correctAnswer);
-    });
-});
 
 describe('create a round object', function(){
     it('should be a function', function(){
         expect(createRound).to.be.a('function')
     });
     it('currentCard should be the first card', function(){
-        //add data values here
-    })
-})
+        const card1 = createCard(0);
+        const card2 = createCard(1);
+        const card3 = createCard(2);
+        const card4 = createCard(3);
+        const card5 = createCard(4);
+        const card6 = createCard(5);
+
+        const deck = [card1, card2, card3, card4, card5];
+        const testRound = createRound(deck);
+        expect(testRound.currentCard).to.equal(card1);
+    });
+    it('should return an object', function(){
+        const card1 = createCard(0);
+        const card2 = createCard(1);
+        const card3 = createCard(2);
+        const card4 = createCard(3);
+        const card5 = createCard(4);
+        const card6 = createCard(5);
+
+        const deck = [card1, card2, card3, card4, card5];
+        const testRound = createRound(deck);
+        expect(testRound).to.be.a('object')
+    });
+});
 
 describe('make updates when a turn is played', function(){
     it('should be a function', function(){
         expect(takeTurn).to.be.a('function')
     });
     it('should update turns count in round', function(){
-      //add data values here  
-    })
+        const card1 = createCard(0);
+        const card2 = createCard(1);
+        const card3 = createCard(2);
+        const card4 = createCard(3);
+        const card5 = createCard(4);
+        const card6 = createCard(5);
+
+        const deck = [card1, card2, card3, card4, card5];
+        const testRound = createRound(deck);
+        const guess = 'object';
+        takeTurn(guess, testRound);
+        expect(testRound.turns).to.equal(1);
+    });
 });
 
 describe('calculate correct guess percentage', function(){
@@ -47,7 +60,22 @@ describe('calculate correct guess percentage', function(){
         expect(calculatePercentCorrect).to.be.a('function')
     });
     it('should return percentage correct', function(){
-        //create values where roundObj.incorrect guesses equals 2, and turns equals 10, return value should be 80%
+        const card1 = createCard(0);
+        const card2 = createCard(1);
+        const card3 = createCard(2);
+        const card4 = createCard(3);
+        const card5 = createCard(4);
+        const card6 = createCard(5);
+        const deck = [card1, card2, card3, card4, card5];
+        const newObj = {
+            deck: [card1, card2, card3, card4, card5], 
+            currentCard: deck[0],
+            incorrectGuesses: 1, 
+            incorrectGuessId: [],
+            turns: 3 
+        }
+        const total = calculatePercentCorrect(newObj);
+        expect(total).to.equal(66.66666666666667)
     })
 });
 
@@ -55,7 +83,4 @@ describe('once cards run out, round ends', function(){
     it('should be a function', function(){
         expect(endRound).to.be.a('function')
     });
-    it('should log that the round is over and how many correct questions', function(){
-        //how to test a console log?
-    })
-})
+});
