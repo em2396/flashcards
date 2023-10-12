de494575263267f17006b933b783188a146d6309
Emalee Poellot <emalee.poellot@gmail.com>
2023-10-12 14:59:00 -0600

    Remove console logs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 var percentageCorrect; 
 
-// const evaluateGuess = (guess, cardObject) => {
-//     if (guess === cardObject.cardAnswer) {
-//         return 'Correct!';
-//     } else {
-//         return 'Incorrect!';
-//     }
-// };
-
 const createRound = currentDeck => {
     const newObj = {
         deck: currentDeck, 
         currentCard: currentDeck[0],
-        incorrectGuesses: 0, //increment
+        incorrectGuesses: 0, 
         incorrectGuessId: [],
-        turns: 0 //increment
+        turns: 0 
     }
-   // console.log(newObj.currentCard);
     return newObj;
 };
 
@@ -33,27 +24,31 @@ const takeTurn = (guess, roundObj) => {
         roundObj.currentCard = roundObj.deck[roundObj.turns];
         return 'Incorrect!';
     } else if (guess === roundObj.currentCard.correctAnswer) {
+        roundObj.turns++;
         return 'Correct!';
     } else {
+        roundObj.turns++;
+        roundObj.incorrectGuesses++;
+        roundObj.incorrectGuessId.push(roundObj.currentCard.id);
         return 'Incorrect!';
-    }
+    };
 };
 
 const calculatePercentCorrect = roundObj => {
-    percentageCorrect =  1 - (roundObj.incorrectGuesses/roundObj.turns);
+    percentageCorrect =  (1 - (roundObj.incorrectGuesses/roundObj.turns)) * 100;
     return percentageCorrect; 
-}; //need to update this so it's a percentage not a decimal.
+}; 
 
 const endRound = roundObj => {
-    if (roundObj.turns === roundObj.deck.length) {
-        return `Round over! You answered ${percentageCorrect} of the questions correctly!`;
-    }
-};
+        calculatePercentCorrect(roundObj);
+        if (roundObj.turns === roundObj.deck.length) {
+            console.log(`Round over! You answered ${percentageCorrect}% of the questions correctly!`)
+        };
+    };
 
 module.exports = { 
     createRound,
     takeTurn,
     calculatePercentCorrect, 
-    endRound, 
-    //evaluateGuess
+    endRound
  }
